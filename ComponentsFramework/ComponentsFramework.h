fd2389fea4d9a0b32838f7982948860adcc1a665
Parul Vats <parul.vats@radiuz.nl>
2020-07-21 00:33:27 +0200

    Initial Commit

---
+++ ComponentsFramework/ComponentsFramework.h
@@ -0,0 +1,19 @@
+//
+//  ComponentsFramework.h
+//  ComponentsFramework
+//
+//  Created by Parul Vats on 21/07/2020.
+//  Copyright © 2020 Tekhsters. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ComponentsFramework.
+FOUNDATION_EXPORT double ComponentsFrameworkVersionNumber;
+
+//! Project version string for ComponentsFramework.
+FOUNDATION_EXPORT const unsigned char ComponentsFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ComponentsFramework/PublicHeader.h>
+
+
